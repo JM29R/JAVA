759e5124ec03401118ea58fac5a8b70b1fc2feac
Juan Manuel Rios <juanmanuelrios5602@gmail.com>
2026-02-08 21:43:55 -0300

    Add files via upload

---
+++ IntegracionAI/src/main/java/com/JMR/IntegracionAI/IntegracionAiApplication.java
@@ -0,0 +1,14 @@
+package com.JMR.IntegracionAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegracionAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegracionAiApplication.class, args);
+	}
+
+
+}

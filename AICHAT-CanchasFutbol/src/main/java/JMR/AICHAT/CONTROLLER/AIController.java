65610ccb6b7d4582729f485c217c9321d58d47ab
JM29R <juanmanuelrios5602@gmail.com>
2026-02-17 09:40:42 -0300

    proyecto chat

--- AICHAT-CanchasFutbol/src/main/java/JMR/AICHAT/CONTROLLER/AIController.java
+++ AICHAT-CanchasFutbol/src/main/java/JMR/AICHAT/CONTROLLER/AIController.java
@@ -2,17 +2,10 @@ package JMR.AICHAT.CONTROLLER;
 
 
 import JMR.AICHAT.InteligenciaArtificial.MensajeRequest;
-import JMR.AICHAT.RESERVAS.DatosReserva;
-import JMR.AICHAT.InteligenciaArtificial.DatosReservaAI;
-import JMR.AICHAT.RESERVAS.Reserva;
 import JMR.AICHAT.SERVICE.GroqService;
-import JMR.AICHAT.SERVICE.ReservaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
 
 @RestController
 @RequestMapping("/ai")
@@ -27,9 +20,23 @@ public class AIController {
     public ResponseEntity<String> reservarConAI(@RequestBody MensajeRequest request) {
 
         String Intencion = groqService.InterpretarIntencion(request);
-        String mensaje = groqService.GestionarReserva(request,Intencion);
+        switch (Intencion) {
+            case "CREAR":
+                String respuesta = groqService.CrearReserva(request);
+                return ResponseEntity.ok(respuesta);
+            case "CANCELAR":
+                String respuesta2 = groqService.CancelarReserva(request);
+                return ResponseEntity.ok(respuesta2);
+            case "MODIFICAR":
+                String respuesta3 = groqService.ModificarReserva(request);
+                return ResponseEntity.ok(respuesta3);
+            case "CONSULTAR":
+                String respuesta4 = groqService.ConsultarReserva(request);
+                return ResponseEntity.ok(respuesta4);
+            default:
+                return ResponseEntity.badRequest().build();
+        }
 
-        return ResponseEntity.ok(mensaje);
     }
 
 
--- AICHAT-CanchasFutbol/src/main/java/JMR/AICHAT/SERVICE/GroqService.java
+++ AICHAT-CanchasFutbol/src/main/java/JMR/AICHAT/SERVICE/GroqService.java
@@ -1,7 +1,6 @@
 package JMR.AICHAT.SERVICE;
 
 
-import JMR.AICHAT.CANCHAS.Cancha;
 import JMR.AICHAT.CANCHAS.CanchaRepository;
 import JMR.AICHAT.InteligenciaArtificial.DatosModificarAI;
 import JMR.AICHAT.InteligenciaArtificial.DatosReservaAI;
@@ -65,120 +64,116 @@ public class GroqService {
     }
 
 
-    public String GestionarReserva(MensajeRequest mensajeRequest, String intencion) {
-
-        switch (intencion) {
-            case "CREAR":
-                String prompt = """
-                        Sos un asistente que extrae datos para reservas de canchas.
-                        Devuelve SOLO JSON válido, sin texto adicional, sin explicaciones, sin comillas invertidas.
-                                    Reglas:
-                                        - Si dice "hoy", usar la fecha de hoy
-                                        - Si dice "mañana", usar la fecha de mañana
-                                        - Si dice un día de la semana (ej: jueves), calcular la próxima fecha
-                                        - Si la hora es "9", interpretarla como 09:00
-                                        - Si no especifica cancha, usar canchaId = 1
-                                        - Formato de fecha: YYYY-MM-DD
-                                        - Formato de hora: HH:mm
-                        
-                        Convertí el mensaje del usuario a JSON con este formato:
-                        
-                        {
-                          "canchaId": number,
-                          "fecha": "YYYY-MM-DD",
-                          "hora": "HH:MM"
-                        }
-                        
-                        Si falta un dato, dejalo null.
-                        Respondé SOLO el JSON. Sin texto extra.
-                        
-                        Mensaje:
-                        """ + mensajeRequest.mensaje();
+        public String CrearReserva(MensajeRequest mensajeRequest) {
+            String prompt = """
+                Sos un asistente que extrae datos para reservas de canchas.
+                Devuelve SOLO JSON válido, sin texto adicional, sin explicaciones, sin comillas invertidas.
+                            Reglas:
+                                - Si dice "hoy", usar la fecha de hoy
+                                - Si dice "mañana", usar la fecha de mañana
+                                - Si dice un día de la semana (ej: jueves), calcular la próxima fecha
+                                - Si la hora es "9", interpretarla como 09:00
+                                - Si no especifica cancha, usar canchaId = 1
+                                - Formato de fecha: YYYY-MM-DD
+                                - Formato de hora: HH:mm
+                
+                Convertí el mensaje del usuario a JSON con este formato:
+                
+                {
+                  "canchaId": number,
+                  "fecha": "YYYY-MM-DD",
+                  "hora": "HH:MM"
+                }
+                
+                Si falta un dato, dejalo null.
+                Respondé SOLO el JSON. Sin texto extra.
+                
+                Mensaje:
+                """ + mensajeRequest.mensaje();
 
 
-                DatosReservaAI datosAI = chatClient.prompt()
-                        .user(prompt)
-                        .call()
-                        .entity(DatosReservaAI.class);
+            DatosReservaAI datosAI = chatClient.prompt()
+                .user(prompt)
+                .call()
+                .entity(DatosReservaAI.class);
 
-                DatosReserva datos = new DatosReserva(
-                        LocalDate.parse(datosAI.fecha()),
-                        LocalTime.parse(datosAI.hora()),
-                        datosAI.canchaId(),
-                        mensajeRequest.nombre(),
-                        mensajeRequest.telefono()
-                );
-                try {
-                    Reserva reserva = reservaService.reservarCancha(datos);
-                    return "Reserva creada correctamente.";
-                } catch (Exception e) {
+            DatosReserva datos = new DatosReserva(
+                LocalDate.parse(datosAI.fecha()),
+                LocalTime.parse(datosAI.hora()),
+                datosAI.canchaId(),
+                mensajeRequest.nombre(),
+                mensajeRequest.telefono()
+            );
+            try {
+            Reserva reserva = reservaService.reservarCancha(datos);
+            return "Reserva creada correctamente.";
+             } catch (Exception e) {
+
                     if (e.getMessage().contains("ocupado")) {
                         var datosdisponibles = new DatosDisponibilidad(datos.fecha(), datos.canchaId());
                         List<LocalTime> disponibles = reservaService.obtenerHorariosDisponibles(datosdisponibles);
                         return "Ese horario en esa cancha se encuentra ocupado, los horarios disponibles son: " + disponibles;
                     }
                     if (e.getMessage().contains("reservar")) {
-
-
-                        return "numero de cancha no encontrado o no existe" ;
+                        return "numero de cancha no encontrado o no existe";
                     }
+                    return "Error inesperado al crear la reserva: " + e.getMessage();
+            }
+        }
+        public String ModificarReserva(MensajeRequest mensajeRequest){
 
-                }
+            String prompt2 = """
+                    Sos un asistente que extrae datos para modificar reservas.
+                    
+                    Si el usuario quiere cambiar un turno, devolvé SOLO JSON.
+                    
+                    Formato:
+                    
+                    {
+                      "canchaId": number,
+                      "fechaActual": "YYYY-MM-DD",
+                      "horaActual": "HH:MM",
+                      "fechaNueva": "YYYY-MM-DD",
+                      "horaNueva": "HH:MM"
+                    }
+                    
+                    Reglas:
+                    - Si dice un día de la semana, calcular la próxima fecha
+                    - Hora "9" → 09:00
+                    - Si no menciona cancha → canchaId = 1
+                    - Si falta algún dato → null
+                    
+                    Mensaje""" + mensajeRequest.mensaje();
+            DatosModificarAI datosModificar = chatClient.prompt()
+                    .user(prompt2)
+                    .call()
+                    .entity(DatosModificarAI.class);
+            var datosModificados = new DatosModificarReserva(
+                    mensajeRequest.telefono(),
+                    LocalDate.parse(datosModificar.fechaActual()),
+                    LocalTime.parse(datosModificar.horaActual()),
+                    LocalDate.parse(datosModificar.fechaNueva()),
+                    LocalTime.parse(datosModificar.horaNueva()),
+                    datosModificar.canchaId(),
+                    mensajeRequest.nombre()
+            );
+            Reserva reservamodificada = reservaService.modificarReserva(datosModificados);
+            return "Reserva modificada correctamente."+ reservamodificada.toString();
+        }
 
+        public String ConsultarReserva(MensajeRequest mensajeRequest) {
+            var reservaConsultar = reservaRepository.findByTelefonoAndNombreCliente(mensajeRequest.telefono(), mensajeRequest.nombre());
+            if (reservaConsultar == null) {
+                return "No se encontraron reservas";
+            }
 
-            case "MODIFICAR":
-                String prompt2 = """
-                        Sos un asistente que extrae datos para modificar reservas.
-                        
-                        Si el usuario quiere cambiar un turno, devolvé SOLO JSON.
-                        
-                        Formato:
-                        
-                        {
-                          "canchaId": number,
-                          "fechaActual": "YYYY-MM-DD",
-                          "horaActual": "HH:MM",
-                          "fechaNueva": "YYYY-MM-DD",
-                          "horaNueva": "HH:MM"
-                        }
-                        
-                        Reglas:
-                        - Si dice un día de la semana, calcular la próxima fecha
-                        - Hora "9" → 09:00
-                        - Si no menciona cancha → canchaId = 1
-                        - Si falta algún dato → null
-                        
-                        Mensaje""" + mensajeRequest.mensaje();
-                DatosModificarAI datosModificar = chatClient.prompt()
-                        .user(prompt2)
-                        .call()
-                        .entity(DatosModificarAI.class);
-                var datosModificados = new DatosModificarReserva(
-                        mensajeRequest.telefono(),
-                        LocalDate.parse(datosModificar.fechaActual()),
-                        LocalTime.parse(datosModificar.horaActual()),
-                        LocalDate.parse(datosModificar.fechaNueva()),
-                        LocalTime.parse(datosModificar.horaNueva()),
-                        datosModificar.canchaId(),
-                        mensajeRequest.nombre()
-                );
-                Reserva reservamodificada = reservaService.modificarReserva(datosModificados);
-                return "Reserva modificada correctamente.";
+            return "Tenés una reserva el "
+                    + reservaConsultar.getFecha()
+                    + " a las "
+                    + reservaConsultar.getHora();
+        }
 
-
-            case "CONSULTAR":
-                var reservaConsultar = reservaRepository.findByTelefonoAndNombreCliente(mensajeRequest.telefono(),mensajeRequest.nombre());
-                if (reservaConsultar == null) {
-                    return "No se encontraron reservas";
-                }
-
-                return "Tenés una reserva el "
-                        + reservaConsultar.getFecha()
-                        + " a las "
-                        + reservaConsultar.getHora();
-
-
-            case "CANCELAR":
+        public String CancelarReserva(MensajeRequest mensajeRequest){
                 var reservaCancelar = reservaRepository.findByTelefonoAndNombreCliente(mensajeRequest.telefono(),mensajeRequest.nombre());
                  var datosCancelar = new DatosIdentificarReserva(
                         reservaCancelar.getFecha(),
@@ -187,9 +182,6 @@ public class GroqService {
                 );
                 reservaService.EliminarReserva(datosCancelar);
                 return "Reserva cancelada";
-
-
-            default: return "no entendi la solicitud";
         }
     }
-}
+
